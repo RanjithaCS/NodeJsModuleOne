6718f13822258b04d892eb0864694bd763c8b7bb
Ranjitha <ranjithasrinath@gmail.com>
2019-10-15 13:40:27 +0530

    adding file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
-
+var chalk =  require('chalk');
 var srv = http.createServer(function(request, response) {
           response.end("Working as expected !!!!")
             });
 srv.listen(3000, function(){
-    console.log('listening to port : 3000');
+    console.log(chalk.green('listening to port : 3000'));
 });
